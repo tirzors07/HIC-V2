a450115408479c5528280469c3f1aec09b9e29d7
tirzors07 <rtirzo@uabc.edu.mx>
2024-11-02 16:33:48 -0700

    test de creacion de branch y evitar conflictos en ramas

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
+/* pri*/
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />  {/* Renderizamos el componente App */}
