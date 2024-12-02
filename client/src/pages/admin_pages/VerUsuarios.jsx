f91ed598bda3e65d210fadab2eb383b5b906b95c
Juan Diaz <juan.diaz.medina@uabc.edu.mx>
2024-12-02 00:55:38 -0800

    uwu

--- client/src/pages/admin_pages/VerUsuarios.jsx
+++ client/src/pages/admin_pages/VerUsuarios.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 import axios from "axios"; 
 
 const VerUsuarios = () => {
@@ -9,6 +10,14 @@ const VerUsuarios = () => {
   const [search, setSearch] = useState("");
   const [foundUser, setFoundUser] = useState(null);
   const [userMsgOpen, setUserMsg] = useState(false);
+  const [isRegistering, setRegister] = useState(false);
+
+  //Nuevos datos de registro
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const navigate = useNavigate();
 
   useEffect( () => {
     getUsers();
@@ -26,6 +35,37 @@ const VerUsuarios = () => {
       console.log("Error al obtener usuarios");
     }
   };
+
+  const handleRegisterButton = () => {
+    setRegister(true);
+  };
+
+  const registerAdmin = async() => {
+    if(password != password2){
+      alert("La contraseña debe coincidir");
+      return;
+    }
+    if(password.length > 15){
+      alert("Contraseña de maximo 15 caracteres");
+      return;
+    }
+    try{
+      const response = await axios.post('http://localhost:3000/user/register', {
+          name_: username,
+          email: email,
+          password_: password,
+          matricula: "default",
+          role: "hic_admin"
+      });
+      if (response.data.success){
+          alert("Usuario creado exitosamente");
+      }
+    } catch(error){
+      alert("Creacion de usuario fallida");
+      console.error("Error durante creación de usuario: ", error);
+    }
+    setRegister(false);
+  }
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -67,6 +107,14 @@ const VerUsuarios = () => {
               Buscar
             </button>
         </form>
+
+        <div className="py-3">
+          <button
+            onClick={handleRegisterButton}
+            className="ml-2 p-2 bg-blue-500 text-white, rounded-md">
+            Registrar Nuevo Administrador
+          </button>
+        </div>
 
         <ul className="w-full max-w-md bg-white shadow-md rounded-lg p-4">
           {users.map((user) => (
@@ -116,6 +164,41 @@ const VerUsuarios = () => {
             </div> 
           </div>
       )}
+
+        {isRegistering && (
+          <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto flex justify-center">
+          <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-lg mx-auto h-auto max-h-screen my-64">
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                registerAdmin();
+            }}>
+                <h2 className="text-2xl font-bold mb-4">Registro de Administrador</h2>
+                <input
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    type="text" name="nombreUsuario" id="nombreUsuario" placeholder="Ingrese su nombre" 
+                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 bg-white my-1 text-black"/>
+                <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email" name="correo" id="correo" placeholder="Ingrese su correo electronico"
+                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500  my-1 bg-white text-black"/>
+                <input
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="text" name="matricula" id="matricula" placeholder="Ingrese una contraseña"
+                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 my-1 bg-white text-black"/>
+                <input
+                    value={password2}
+                    onChange={(e) => setPassword2(e.target.value)}
+                    type="text" name="matricula2" id="matricula2" placeholder="Ingrese la contraseña de nuevo, debe coincidir"
+                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 bg-white text-black my-1"/>
+                <input type="submit" className="btn-register w-full bg-blue-500 text-white py-2 px-2 rounded-md hover:bg-blue-600 transition my-2" value="Registrar Administrador" />
+            </form>
+          </div>
+          </div>
+        )}
+
       </div>
     );
   };
