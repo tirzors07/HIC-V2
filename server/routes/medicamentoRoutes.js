af09c073d2d6d2bfa43d904884526d086bae60a3
Juan Diaz <juan.diaz.medina@uabc.edu.mx>
2024-12-03 01:16:16 -0800

    666

--- server/routes/medicamentoRoutes.js
+++ server/routes/medicamentoRoutes.js
@@ -2,13 +2,6 @@ import express from "express";//para facilitar conexion con el servidor
 import { getAllMedicamentos, getMedicamento, createMedicamento, updateMedicamento, deleteMedicamento } from "../controllers/MedicamentoControllers.js";
 const router = express.Router();
 
-router.post('/new_med', createMedicamento);
-
-export default router;import express from 'express';
-import { getAllMedicamentos, getMedicamento, createMedicamento, updateMedicamento, deleteMedicamento } from "../controllers/MedicamentoControllers.js";
-
-const router = express.Router();
-
 router.get("/medicamentos", getAllMedicamentos);
 router.get("/medicamento/:id", getMedicamento);
 router.post("/new_med", createMedicamento);  // Cambio aquí de /medicamento a /medicamentos
